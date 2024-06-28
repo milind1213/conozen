2c90cda406eb09b05acd499f253976679ca74157
milind1213 <gmilind12@gmail.com>
2024-06-29 02:24:52 +0530

    update

--- src/main/java/com/convozen/Pages/Playwrights/ConvozenWebLogin.java
+++ src/main/java/com/convozen/Pages/Playwrights/ConvozenWebLogin.java
@@ -23,6 +23,7 @@ public class ConvozenWebLogin extends CommonPlaywright {
         page.waitForSelector(emailField);
         waitFor(5);
         try {
+            page.waitForSelector("//*[@id='email' or @placeholder='example@abc.com']", new Page.WaitForSelectorOptions().setTimeout(60000));
             log("Entering username: "+email);
             fill(emailField, email);
 
